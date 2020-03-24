30a9bad6dacb937e231cedf803ddbb1e4d820253
stockerssld <35477074+stockerssld@users.noreply.github.com>
2020-03-24 09:38:56 -0600

    23

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {useState,useEffect} from 'react'
-
+import {Level, Heading, Title} from 'rbx'
 import ReactDOM from 'react-dom'
-import styled from 'styled-components'
+import "rbx/index.css";
 
 function App(){
     
     return(
         <>
-            Hola Mundo
+          Hola
         </>)
 }
 if(document.getElementById('react_root')){
